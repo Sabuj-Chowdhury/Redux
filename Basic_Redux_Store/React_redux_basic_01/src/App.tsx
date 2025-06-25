e2b0e54901fc2f9c9137148b4275a1b7244806df
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-25 20:41:13 +0530

    Actions in Redux.

--- Basic_Redux_Store/React_redux_basic_01/src/App.tsx
+++ Basic_Redux_Store/React_redux_basic_01/src/App.tsx
@@ -1,9 +1,22 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div>
-      <button>Increase</button>
+      <button onClick={handleIncrement}>Increase</button>
       <div>0</div>
-      <button>Decrease</button>
+      <button onClick={handleDecrement}>Decrease</button>
     </div>
   );
 }
