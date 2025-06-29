c2d020f60424a90f7d694c9d2c43849f817a1618
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-29 18:59:42 +0530

    handle types for task data

--- Basic_TODO_Redux_with_shadcn/src/redux/features/task/taskSlice.ts
+++ Basic_TODO_Redux_with_shadcn/src/redux/features/task/taskSlice.ts
@@ -1,7 +1,7 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/taskTypes";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+import { createSlice, type PayloadAction, nanoid } from "@reduxjs/toolkit";
+// import { v4 as uuidv4 } from "uuid";
 
 interface IInitialState {
   tasks: ITask[];
@@ -13,18 +13,29 @@ const initialState: IInitialState = {
   filter: "all",
 };
 
+type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">;
+
+const createTask = (taskData: DraftTask): ITask => {
+  return { id: nanoid(), isCompleted: false, ...taskData };
+};
+
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
     addTask: (state, action: PayloadAction<ITask>) => {
       // state.tasks.push(action.payload);
-      const id = uuidv4();
-      const taskData = {
-        ...action.payload,
-        id,
-        isCompleted: false,
-      };
+
+      // way : 1
+      // const id = uuidv4();
+      // const taskData = {
+      //   ...action.payload,
+      //   id,
+      //   isCompleted: false,
+      // };
+
+      // way : 2
+      const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
   },
