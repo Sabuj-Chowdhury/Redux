36d195676a58a046dfc25b54e4ffd8421f503bf0
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-30 09:08:40 +0530

    user reducer add in the redux store

--- Basic_TODO_Redux_with_shadcn/src/redux/store.ts
+++ Basic_TODO_Redux_with_shadcn/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./features/task/taskSlice";
+import userReducer from "./features/user/userSlice";
 
 const store = configureStore({
   reducer: {
     todo: taskReducer,
+    users: userReducer,
   },
 });
 
