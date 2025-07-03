accc7042a76a3365f318dfd20ed986d9b6a6f100
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-03 16:56:04 +0530

    creating API for RTK Query

--- RTK query/RTK Query(Client)/src/redux/features/user/userSlice.ts
+++ RTK query/RTK Query(Client)/src/redux/features/user/userSlice.ts
@@ -1,39 +1,11 @@
-import type { RootState } from "@/redux/store";
-import type { IUser } from "@/taskTypes";
-import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-interface IInitialState {
-  user: IUser[];
-}
-
-const initialState: IInitialState = {
-  user: [],
-};
-
-type DraftUser = Pick<IUser, "name">;
-
-const createUser = (userData: DraftUser): IUser => {
-  return { id: nanoid(), ...userData };
-};
-
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    addUser: (state, action: PayloadAction<IUser>) => {
-      const userData = createUser(action.payload);
-      state.user.push(userData);
-    },
-    deleteUser: (state, action: PayloadAction<string>) => {
-      state.user = state.user.filter((user) => user.id !== action.payload);
-    },
-  },
+export const baseAPI = createApi({
+  reducerPath: "baseAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  endpoints: (build) => ({
+    getTask: build.query({
+      query: () => "/tasks",
+    }),
+  }),
 });
-
-export const selectUser = (state: RootState) => {
-  return state.users.user;
-};
-
-export const { addUser, deleteUser } = userSlice.actions;
-
-export default userSlice.reducer;
